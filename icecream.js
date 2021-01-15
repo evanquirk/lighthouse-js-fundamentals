a62cd95bc6985b584cbf537ab93d400c90607032
Evan Quirk <evan_quirk@me.com>
2021-01-15 23:14:17 +0000

    array examples

---
+++ icecream.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push ("root beer");
+console.log(iceCreamFlavours[5]);  
+console.log("Bob" || " Marley")
+console.log (5 * "3");
+
+
