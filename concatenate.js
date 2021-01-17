9fb0bada5d8dab0dfd4d92bdc0136474c793bc7b
Evan Quirk <evan_quirk@me.com>
2021-01-17 06:20:58 +0000

    remove sample console.log from file

--- concatenate.js
+++ concatenate.js
@@ -10,8 +10,3 @@ const concat = function(array1, array2) {
   }
   return joined
 }
-
-console.log(concat([ "yellow", 2, 3 ], [ 2, 4, 5, 6 ]), "=?", [ "yellow", 2, 3, 4, 5, 6 ]);
-console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
-console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
-console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
